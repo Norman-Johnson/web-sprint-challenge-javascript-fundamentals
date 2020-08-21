53def954727a3a8c9fdafae131a4550ef9536e9d
Norman Johnson <norman-johnson@lambdastudents.com>
2020-08-21 11:02:31 -0500

    Task 1

--- challenges/arrays-callbacks.js
+++ challenges/arrays-callbacks.js
@@ -20,7 +20,13 @@ const zooAnimals = [
 The zoos want to display both the scientific name and the animal name in front of the habitats. Populate the displayNames array with only the animal_name and scientific_name of each animal. displayNames will be an array of strings, and each string should follow this pattern: "Name: Jackal, asiatic, Scientific: Canis aureus."
 
 */
-const displayNames = [];
+console.log('********** Task 1 **********')
+
+const displayNames = [animal_name, scientific_name];
+
+displayNames.forEach(function(zooAnimals){
+
+});
 console.log(displayNames);
 
 /* Request 2: .map()
